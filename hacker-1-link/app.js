faed9e5fde03416de84ea079723d551d58efcc39
ayush narula <developeron29@gmail.com>
2014-04-15 21:25:36 +0530

    done

--- hacker-1-link/app.js
+++ hacker-1-link/app.js
@@ -6,16 +6,16 @@ broken = [],
 linkNo = 0,
 brokenNo = 0;
 
-async.waterfall([
+async.waterfall([ // Async helps in control-flow during asynchronous requests
 
 	function (callback){
-		request('http://www.mobileacademy.com/', function (error, response, body) {
+		request('http://www.mobileacademy.com/', function (error, response, body) { // Requests Mobileacademy's website
 	if(!error && response.statusCode == 200) {
 		$ = cheerio.load(body);
-		$('a').each(function () {
+		$('a').each(function () { //gets <a> tags
 			var link = $(this);
 			linkNo++;
-			links.push(link.attr('href'));
+			links.push(link.attr('href')); //Gets links
 		});
 		callback(null,links,linkNo);
 		
@@ -25,11 +25,11 @@ async.waterfall([
 function (links,linkNo, callback) {
 	brokenNo = 0;
 	for(var i=0;i<links.length;i++){
-		request(links[i], function (error, response, body) {
+		request(links[i], function (error, response, body) { // Calls individual links to check if broken
 
-				if(response && response.statusCode != 200) {
-					brokenNo++;
-					broken.push(links[i]);
+				if(response && response.statusCode != 200) { //Checks for broken links
+					brokenNo++; 
+					broken.push(links[i]); // appends broken links
 				}
 			});
 	}
@@ -43,14 +43,14 @@ function (links,linkNo,brokenNo,broken,callback) {
 		console.log('Broken Links: '+brokenNo+'\n');
 		console.log('Link,Broken');
 		for(var i = 0; i < broken.length; i++) {
-			console.log(broken[i],'True');
+			console.log(broken[i],'True'); // Prints broken links
 			var index = links.indexOf(broken[i]);
 			if(index > -1) {
-				links.splice(index, 1);
+				links.splice(index, 1); //Removes broken links from all links
 			}
 		} 
 		for(var j= 0 ; j < links.length; j++){
-			console.log(links[j],'False');
+			console.log(links[j],'False'); // Prints non-broken links
 		}
 		callback(null,'done');
 }
